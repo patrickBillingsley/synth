a7c33c779110f5e5558ef8fe9aac5c8ff8a1a1bb
Patrick Billingsley <Patrick@Kristens-iMac.local>
2020-11-14 16:25:19 -0600

    Refactor Oscillator Class into Sub Classes
    *Add lfo, tremolo, and vibrato classes

--- app.js
+++ app.js
@@ -1,48 +1,71 @@
-class Sound {
-    constructor(context) {
+class Oscillator {
+    constructor(context, now) {
         this.context = context;
-    }
-    init() {
+        this.now = now;
         this.oscillator = this.context.createOscillator();
         this.gainNode = this.context.createGain();
         this.oscillator.connect(this.gainNode);
+        this.gainNode.gain.value = 0.5;
+        this.oscillator.start();
+    }
+    play(frequency) {
+        this.oscillator.frequency.exponentialRampToValueAtTime(frequency, this.now);
         this.gainNode.connect(this.context.destination);
-        this.oscillator.type = 'sine';
     }
-    play(value, time) {
-        this.init();
-        this.oscillator.frequency.value = value;
-        this.gainNode.gain.setValueAtTime(0.001, this.context.currentTime);
-        this.gainNode.gain.exponentialRampToValueAtTime(1, this.context.currentTime + 0.5);
-        this.oscillator.start(time);
-        this.stop(time);
-    }
-    stop(time) {
-        this.gainNode.gain.exponentialRampToValueAtTime(0.001, time + 1);
-        this.oscillator.stop(time + 1);
+    stop() {
+        this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.now + 1);
+        this.gainNode.disconnect(this.context.destination);
+        this.gainNode.gain.setValueAtTime(oscillatorOneVolume.value/100, this.now + 1.1);
     }
 }
-
+class Lfo extends Oscillator {
+    constructor(context, now) {
+        super(context, now);
+        this.oscillator.frequency.value = 5;
+    }
+}
+class Vibrato extends Lfo {
+    constructor(context, now, osc) {
+        super(context, now);
+        this.gainNode.gain.value = 20;
+        this.gainNode.connect(osc.oscillator.frequency);
+    }
+}
+class Tremolo extends Lfo {
+    constructor(context, now, osc) {
+        super(context, now);
+        this.gainNode.gain.value = 0.15;
+        this.gainNode.connect(osc.gainNode.gain);
+    }
+}
 
 const keys = document.querySelectorAll('[data-note]');
 
-const notes = {
-    c4: 261.63,
-    d4: 293.66,
-    e4: 329.63,
-    f4: 349.23,
-    g4: 392.00,
-    a5: 440.00,
-    b5: 493.88,
-    c5: 523.25
-}
+const notes = { c4: 261.63, d4: 293.66, e4: 329.63, f4: 349.23,
+                g4: 392.00, a5: 440.00, b5: 493.88, c5: 523.25 }
+
+const context = new (window.AudioContext || window.webkitAudioContext)();
+const now = context.currentTime;
+
+const oscillatorOne = new Oscillator(context, now);
+const oscillatorOneVibrato = new Vibrato(context, now, oscillatorOne);
+const oscillatorOneTremolo = new Tremolo(context, now, oscillatorOne);
+
+const oscillatorOneVolume = document.getElementById('oscillatorOneVolume');
+const oscillatorOneLfoRandomness = document.getElementById('oscillatorOneLfoRandomness');
+
 
 keys.forEach(key => {
-    key.addEventListener('click', event => {
+    key.addEventListener('mousedown', event => {
         const frequency = notes[event.target.dataset.note];
-        const context = new (window.AudioContext || window.webkitAudioContext)();
-        const note = new Sound(context);
-        const now = context.currentTime;
-        note.play(frequency, now);
+        oscillatorOne.play(frequency);
     })
+    key.addEventListener('mouseup', () => {
+        oscillatorOne.stop();
+    })
+})
+
+oscillatorOneVolume.addEventListener('input', event => {
+    const value = event.target.value;
+    oscillatorOne.gainNode.gain.setValueAtTime(value/100, oscillatorOne.context.currentTime);
 })
