b508b0bb8f383da7f28417d961502599b07491d4
Patrick Billingsley <Patrick@Kristens-iMac.local>
2020-12-08 15:08:00 -0600

    Computer Swap

--- app.js
+++ app.js
@@ -10,22 +10,25 @@ window.inputKnobsOptions = { knobDiameter: '100' };
 class Oscillator {
     constructor(outDest, vol, freq, range, position) {
         this.outDest = outDest;
-        this.range = Number(range);
-        this.freq = Number(freq);
+        this.range = range;
+        this.freq = freq;
 
         this.osc = context.createOscillator();
         this.lvl = context.createGain();
-        this.lvl.gain.setValueAtTime(vol, now);
+
+        this.lvl.gain.setValueAtTime(vol, context.currentTime);
+
         this.osc.connect(this.lvl);
         this.lvl.connect(outDest);
+
         this.osc.start();
     }
     play(freq) {
-        this.osc.frequency.cancelScheduledValues(now);
-        this.osc.frequency.linearRampToValueAtTime(freq, now);
+        this.osc.frequency.cancelScheduledValues(context.currentTime);
+        this.osc.frequency.linearRampToValueAtTime(freq, context.currentTime + controllers.glide.value);
     }
-}
-class MasterVolume {
+})
+class Master {
     constructor() {
         this.lvl = context.createGain();
         this.lvl.gain.value = 0;
@@ -38,14 +41,14 @@ class MasterVolume {
         } else {
             this.lvl.gain.setValueAtTime(0.001, context.currentTime);
         }
-        this.lvl.gain.exponentialRampToValueAtTime(this.vol, context.currentTime + loudnessAttack);
+        this.lvl.gain.exponentialRampToValueAtTime(this.vol, context.currentTime);
     }
     stop() {
         if(this.lvl.gain.value) {
             this.lvl.gain.setValueAtTime(this.lvl.gain.value, context.currentTime);
             this.lvl.gain.cancelScheduledValues(context.currentTime + 0.001);
-            this.lvl.gain.exponentialRampToValueAtTime(0.001, context.currentTime + loudnessDecay);
-            this.lvl.gain.setValueAtTime(0, context.currentTime + loudnessDecay);
+            this.lvl.gain.exponentialRampToValueAtTime(0.001, context.currentTime);
+            this.lvl.gain.setValueAtTime(0, context.currentTime);
         }
     }
 }
@@ -66,16 +69,18 @@ class LFO {
     }
 }
 class Knob {
-    constructor(elem) {
+    constructor(elem, slave, property) {
         this.elem = elem;
         this.value = elem.value;
+        this.property = property;
+        this.slave = slave;
         this.addListener();
         this.pushToArray();
     }
     addListener() {
         this.elem.addEventListener('input', event => {
             this.value = event.target.value;
-            console.log(this.elem.name + ' ' + this.value);
+            console.log(this.name + ' = ' + this.value);
         })
     }
     pushToArray() {
@@ -83,8 +88,8 @@ class Knob {
     }
 }
 class Range extends Knob {
-    constructor(elem) {
-        super(elem);
+    constructor(elem, slave, property) {
+        super(elem, slave, property);
         this.waveformArray = ['sine', 'square', 'sawtooth', 'triangle', 'square', 'sine'];
 
         this.updateWaveform(elem);
@@ -95,9 +100,11 @@ class Range extends Knob {
 }
 
 class Switch {
-    constructor(elem) {
+    constructor(elem, slave, property) {
         this.elem = elem;
         this.value = elem.value;
+        this.slave = slave;
+        this.property = property;
         this.addListener();
         this.pushToArray();
     }
@@ -147,96 +154,137 @@ const switchArray = [];
 
 //----------  CONTROLLERS  ----------
 
+    //----- knobs -----
+
 const oscOneFreqElem = document.querySelector('[name="osc-one-freq"]');
-const oscOneFreq = new Knob(oscOneFreqElem);
-
 const glideElem = document.querySelector('[name="glide"]');
-const glide = new Knob(glideElem);
-
 const modMixElem = document.querySelector('[name="mod-mix"]');
-const modMix = new Knob(modMixElem);
+
+    //----- switches -----
 
 const oscFilterSwitchElem = document.querySelector('[name="osc-filter-switch"]');
-const oscFilterSwitch = new Switch(oscFilterSwitchElem);
-
 const noiseLfoSwitchElem = document.querySelector('[name="noise-lfo-switch"]');
+
+const controllers = {
+    oscOneFreq: new Knob(oscOneFreqElem, 'oscOne', 'freq'),
+    glide: new Knob(glideElem, ['oscOne', 'oscTwo', 'oscThree'], 'glide'),
+    modMix: new Knob(modMixElem),
+    oscFilterSwitch: new Switch(oscFilterSwitchElem),
+    noiseLfoSwitch: new Switch(noiseLfoSwitchElem)
+};
+
+const oscOneFreq = new Knob(oscOneFreqElem, 'oscOne', 'freq');
+const glide = new Knob(glideElem, ['oscOne', 'oscTwo', 'oscThree'], 'glide');
+const modMix = new Knob(modMixElem);
+
+const oscFilterSwitch = new Switch(oscFilterSwitchElem);
 const noiseLfoSwitch = new Switch(noiseLfoSwitchElem);
+
 
 
 
 
 //----------  OSCILLATOR BANK  ----------
 
+    //----- range -----
+
 const oscOneRangeElem = document.querySelector('[name="osc-one-range"]');
-const oscOneRange = new Range(oscOneRangeElem);
-
-const oscOneWaveformElem = document.querySelector('[name="osc-one-waveform"]');
-const oscOneWaveform = new Range(oscOneWaveformElem);
-
 const oscTwoRangeElem = document.querySelector('[name="osc-two-range"]');
-const oscTwoRange = new Range(oscTwoRangeElem);
+const oscThreeRangeElem = document.querySelector('[name="osc-three-range"]');
+
+//----- freq -----
 
 const oscTwoFreqElem = document.querySelector('[name="osc-two-freq"]');
-const oscTwoFreq = new Knob(oscTwoFreqElem);
-
-const oscTwoWaveformElem = document.querySelector('[name="osc-two-waveform"]');
-const oscTwoWaveform = new Range(oscTwoWaveformElem);
-
-const oscThreeRangeElem = document.querySelector('[name="osc-three-range"]');
-const oscThreeRange = new Range(oscThreeRangeElem);
-
 const oscThreeFreqElem = document.querySelector('[name="osc-three-freq"]');
-const oscThreeFreq = new Knob(oscThreeFreqElem);
 
+//----- waveform -----
+
+const oscOneWaveformElem = document.querySelector('[name="osc-one-waveform"]');
+const oscTwoWaveformElem = document.querySelector('[name="osc-two-waveform"]');
 const oscThreeWaveformElem = document.querySelector('[name="osc-three-waveform"]');
-const oscThreeWaveform = new Range(oscThreeWaveformElem);
+
+//----- switches -----
 
 const oscModSwitchElem = document.querySelector('[name="osc-mod-switch"]');
-const oscModSwitch = new Switch(oscModSwitchElem);
+const oscThreeControlSwitchElem = document.querySelector('[name="osc-three-control-switch"]');
+
+const oscBank = {
+    oscOneRange: new Range(oscOneRangeElem, 'oscOne', 'range'),
+    oscTwoRange: new Range(oscTwoRangeElem, 'oscTwo', 'range'),
+    oscThreeRange: new Range(oscThreeRangeElem, 'oscThree', 'range'),
+    oscTwoFreq: new Knob(oscTwoFreqElem, 'oscTwo', 'freq'),
+    oscThreeFreq: new Knob(oscThreeFreqElem, 'oscThree', 'freq'),
+    oscOneWaveform: new Range(oscOneWaveformElem, 'oscOne', 'waveform'),
+    oscTwoWaveform: new Range(oscTwoWaveformElem, 'oscTwo', 'waveform'),
+    oscThreeWaveform: new Range(oscThreeWaveformElem, 'oscThree', 'waveform'),
+    oscModSwitch: new Switch(oscModSwitchElem, ['oscOne', 'oscTwo', 'oscThree'], 'mod'),
+    oscThreeControlSwitch: new Switch(oscThreeControlSwitchElem, 'oscThree', 'control')
+}
+
+const oscOneRange = new Range(oscOneRangeElem, 'oscOne', 'range');
+const oscTwoRange = new Range(oscTwoRangeElem, 'oscTwo', 'range');
+const oscThreeRange = new Range(oscThreeRangeElem, 'oscThree', 'range');
+
+const oscTwoFreq = new Knob(oscTwoFreqElem, 'oscTwo', 'freq');
+const oscThreeFreq = new Knob(oscThreeFreqElem, 'oscThree', 'freq');
+
+const oscOneWaveform = new Range(oscOneWaveformElem, 'oscOne', 'waveform');
+const oscTwoWaveform = new Range(oscTwoWaveformElem, 'oscTwo', 'waveform');
+const oscThreeWaveform = new Range(oscThreeWaveformElem, 'oscThree', 'waveform');
 
 const oscThreeControlSwitchElem = document.querySelector('[name="osc-three-control-switch"]');
-const oscThreeControlSwitch = new Switch(oscThreeControlSwitchElem);
+const oscModSwitch = new Switch(oscModSwitchElem, ['oscOne', 'oscTwo', 'oscThree'], 'mod');
 
 
 
 
 //----------  MIXER  ----------
 
+    //----- knobs -----
+
 const oscOneVolElem = document.querySelector('[name="osc-one-vol"]');
-const oscOneVol = new Knob(oscOneVolElem);
-
-
-const extInputVolElem = document.querySelector('[name="ext-input-vol"]');
-const extInputVol = new Knob(extInputVolElem);
-
 const oscTwoVolElem = document.querySelector('[name="osc-two-vol"]');
-const oscTwoVol = new Knob(oscTwoVolElem);
-
-const noiseVolElem = document.querySelector('[name="noise-vol"]');
-const noiseVol = new Knob(noiseVolElem);
-
 const oscThreeVolElem = document.querySelector('[name="osc-three-vol"]');
-const oscThreeVol = new Knob(oscThreeVolElem);
+const extInputVolElem = document.querySelector('[name="ext-input-vol"]');
+const noiseVolElem = document.querySelector('[name="noise-vol"]');
 
-
+//----- switches -----
 
 const oscOneSwitchElem = document.querySelector('[name="osc-one-switch"]');
-const oscOneSwitch = new Switch(oscOneSwitchElem);
-
-const extInputSwitchElem = document.querySelector('[name="ext-input-switch"]');
-const extInputSwitch = new Switch(extInputSwitchElem);
-
 const oscTwoSwitchElem = document.querySelector('[name="osc-two-switch"]');
-const oscTwoSwitch = new Switch(oscTwoSwitchElem);
-
-const noiseSwitchElem = document.querySelector('[name="noise-switch"]');
-const noiseSwitch = new Switch(noiseSwitchElem);
-
 const oscThreeSwitchElem = document.querySelector('[name="osc-three-switch"]');
-const oscThreeSwitch = new Switch(oscThreeSwitchElem);
-
+const extInputSwitchElem = document.querySelector('[name="ext-input-switch"]');
+const noiseSwitchElem = document.querySelector('[name="noise-switch"]');
 const whitePinkSwitchElem = document.querySelector('[name="white-pink-switch"]');
-const whitePinkSwitch = new Switch(whitePinkSwitchElem);
+
+
+const mixer = {
+    oscOneVol: new Knob(oscOneVolElem, 'oscOne', 'vol'),
+    oscTwoVol: new Knob(oscTwoVolElem, 'oscTwo', 'vol'),
+    oscThreeVol: new Knob(oscThreeVolElem, 'oscThree', 'vol'),
+    extInputVol: new Knob(extInputVolElem, 'extInput', 'vol'),
+    noiseVol: new Knob(noiseVolElem, 'noise', 'vol'),
+
+    oscOneSwitch: new Switch(oscOneSwitchElem, 'oscOne', 'on'),
+    oscTwoSwitch: new Switch(oscTwoSwitchElem, 'oscTwo', 'on'),
+    oscThreeSwitch: new Switch(oscThreeSwitchElem, 'oscThree', 'on'),
+    extInputSwitch: new Switch(extInputSwitchElem, 'extInput', 'on'),
+    noiseSwitch: new Switch(noiseSwitchElem, 'noise', 'on'),
+    whitePinkSwitch: new Switch(whitePinkSwitchElem, 'whitePink', 'on')
+}
+
+const oscOneVol = new Knob(oscOneVolElem, 'oscOne', 'vol');
+const oscTwoVol = new Knob(oscTwoVolElem, 'oscTwo', 'vol');
+const oscThreeVol = new Knob(oscThreeVolElem, 'oscThree', 'vol');
+const extInputVol = new Knob(extInputVolElem, 'extInput', 'vol');
+const noiseVol = new Knob(noiseVolElem, 'noise', 'vol');
+
+const oscOneSwitch = new Switch(oscOneSwitchElem, 'oscOne', 'on');
+const oscTwoSwitch = new Switch(oscTwoSwitchElem, 'oscTwo', 'on');
+const oscThreeSwitch = new Switch(oscThreeSwitchElem, 'oscThree', 'on');
+const extInputSwitch = new Switch(extInputSwitchElem, 'extInput', 'on');
+const noiseSwitch = new Switch(noiseSwitchElem, 'noise', 'on');
+const whitePinkSwitch = new Switch(whitePinkSwitchElem, 'whitePink', 'on');
 
 
 
@@ -244,22 +292,20 @@ const whitePinkSwitch = new Switch(whitePinkSwitchElem);
 //----------  FILTER  ----------
 
 const cutoffFreqElem = document.querySelector('[name="cutoff-freq"]');
-const cutoffFreq = new Knob(cutoffFreqElem);
-
 const emphasisElem = document.querySelector('[name="emphasis"]');
-const emphasis = new Knob(emphasisElem);
-
 const contourElem = document.querySelector('[name="contour"]');
-const contour = new Knob(contourElem);
-
 const filterAttackElem = document.querySelector('[name="filter-attack"]');
-const filterAttack = new Knob(filterAttackElem);
-
 const filterDecayElem = document.querySelector('[name="filter-decay"]');
-const filterDecay = new Knob(filterDecayElem);
-
 const filterSustainElem = document.querySelector('[name="filter-sustain"]');
-const filterSustain = new Knob(filterSustainElem);
+
+const filter = {
+    cutoffFreq: new Knob(cutoffFreqElem, 'filter', 'cutoffFreq'),
+    emphasis: new Knob(emphasisElem, 'filter', 'emphasis'),
+    contour: new Knob(contourElem, 'filter', 'contour'),
+    attack: new Knob(filterAttackElem, 'filter', 'attack'),
+    decay: new Knob(filterDecayElem, 'filter', 'decay'),
+    sustain: new Knob(filterSustainElem, 'filter', 'sustain')
+};
 
 
 
@@ -267,37 +313,43 @@ const filterSustain = new Knob(filterSustainElem);
 //----------  LOUDNESS CONTOUR  ----------
 
 const loudnessAttackElem = document.querySelector('[name="loudness-attack"]');
-const loudnessAttack = new Knob(loudnessAttackElem);
-
 const loudnessDecayElem = document.querySelector('[name="loudness-decay"]');
-const loudnessDecay = new Knob(loudnessDecayElem);
-
 const loudnessSustainElem = document.querySelector('[name="loudness-sustain"]');
-const loudnessSustain = new Knob(loudnessSustainElem);
+
+const loudness = {
+    attack: new Knob(loudnessAttackElem, 'loudness', 'attack'),
+    decay: new Knob(loudnessDecayElem, 'loudness', 'decay'),
+    sustain: new Knob(loudnessSustainElem,'loudness', 'sustain')
+};
+
 
 
 
 //----------  OUTPUT  ----------
 
 const outputVolElem = document.querySelector('[name="output-vol"]');
-const outputVol = new Knob(outputVolElem);
+
+const output = {
+    outputVol: new Knob(outputVolElem, 'output', 'vol')
+}
+
 
 
 
 
 //----------  MASTER SECTION  ----------
 
-const masterVol = new MasterVolume();
-masterVol.lvl.connect(context.destination);
+const master = new Master();
+master.lvl.connect(context.destination);
 
 
 
 
 //----------  OSCILLATORS  ----------
 
-const oscOne = new Oscillator(masterVol.lvl, oscOneVol.value, oscOneFreq.value, oscOneRange.value, oscOneWaveform.value);
-const oscTwo = new Oscillator(masterVol.lvl, oscTwoVol.value, oscTwoFreq.value, oscTwoRange.value, oscTwoWaveform.value);
-const oscThree = new Oscillator(masterVol.lvl, oscThreeVol.value, oscThreeFreq.value, oscThreeRange.value, oscThreeWaveform.value);
+const oscOne = new Oscillator(master.lvl, oscOneVol.value, oscOneFreq.value, oscOneRange.value, oscOneWaveform.value);
+const oscTwo = new Oscillator(master.lvl, oscTwoVol.value, oscTwoFreq.value, oscTwoRange.value, oscTwoWaveform.value);
+const oscThree = new Oscillator(master.lvl, oscThreeVol.value, oscThreeFreq.value, oscThreeRange.value, oscThreeWaveform.value);
 // const lfo = new LFO([oscOne.osc.frequency, oscTwo.osc.frequency]);
 
 
@@ -312,7 +364,7 @@ keys.forEach(key => {
         const note = event.target.dataset.note;
         const octave = Number(event.target.dataset.octave);
 
-            //----- OSC 1 -----
+            //----- osc 1 -----
 
         Object.entries(ranges[oscOne.range + octave]).forEach(arr => {
             if(arr.includes(note)) {
@@ -321,7 +373,7 @@ keys.forEach(key => {
             }
         })
         
-            //----- OSC 2 -----
+            //----- osc 2 -----
 
         Object.entries(ranges[oscTwo.range + octave]).forEach(arr => {
             if(arr.includes(note)) {
@@ -330,7 +382,7 @@ keys.forEach(key => {
             }
         })
 
-            //----- OSC 3 -----
+            //----- osc 3 -----
 
         Object.entries(ranges[oscThree.range + octave]).forEach(arr => {
             if(arr.includes(note)) {
@@ -339,12 +391,12 @@ keys.forEach(key => {
             }
         })
 
-        masterVol.play();
+        master.play();
 
     })
 
     key.addEventListener('mouseout', () => {
-        masterVol.stop();
+        master.stop();
     })
 })
 
@@ -400,7 +452,7 @@ function toObject(keys, values) {
     // oscThree.waveform = selectWaveform(waveformValue);
     oscThree.vol = Number(oscThreeVol.value);
 
-    masterVol.vol = Number(outputVolElem.value);
+    master.vol = Number(outputVolElem.value);
 })();
 
 
@@ -455,3 +507,12 @@ function toObject(keys, values) {
 // };
 
 // draw();
+
+
+
+// switchArray.forEach(object => {
+//     if(object.slave == 'oscThree')
+//     console.log(object.slave + ' ' + object.property + ' = ' + object.value);
+// })
+
+console.log(controllers);
